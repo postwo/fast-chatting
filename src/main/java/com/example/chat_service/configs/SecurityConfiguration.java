fbdeb8b1a07ebf67012157d5e695f94b352e2219
postwo <codo7717@naver.com>
2025-05-12 12:07:55 +0900

    securityconfig 패키지 위치 수정

--- src/main/java/com/example/chat_service/configs/SecurityConfiguration.java
+++ src/main/java/com/example/chat_service/configs/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.chat_service;
+package com.example.chat_service.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
